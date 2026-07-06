08528c50395ba67dc8ae59e2cba0c2b5a02ab014
Pankaj Singh Rawat <qoolphoenix@gmail.com>
2026-07-07 00:01:43 +0530

    script: updates

--- data/stream_simulator.py
+++ data/stream_simulator.py
@@ -1,25 +1,25 @@
-'''
-This script will load the PaySim CSV, sort it by the step column (which represents hours), and yield batches. At step == 150, it will inject covariate shift by multiplying the transaction amount by 3 for legitimate transfers.
-'''
+"""
+This script will load the PaySim CSV, sort it by the step column (which represents hours), and yield batches.
+At step == 150, it will inject covariate shift by multiplying the transaction amount by 3 for legitimate transfers.
+"""
 
 import pandas as pd
 import numpy as np
 
 class DataStreamer:
     def __init__(self, file_path: str, drift_start_step: int = 150):
-        # load data and sort by time step
+        # Load data and sort by time step
         self.df = pd.read_csv(file_path).sort_values(by='step').reset_index(drop=True)
         self.drift_start_step = drift_start_step
-        self.unique_step = self.df['step'].unique()
+        self.unique_steps = self.df['step'].unique()
         self.current_index = 0
 
-
     def get_next_batch(self):
-        if self.current_index >= len(self.unique_step):
-            return None # stram is finished in this scenerio
+        if self.current_index >= len(self.unique_steps):
+            return None  # Stream is finished in this scenario
 
-        step = self.unique_step[self.current_index]
-        batch = self.df[self.df['step']==step].copy()
+        step = self.unique_steps[self.current_index]
+        batch = self.df[self.df['step'] == step].copy()
 
         if step >= self.drift_start_step:
             batch = self._inject_drift(batch)
@@ -27,9 +27,9 @@ class DataStreamer:
         self.current_index += 1
         return batch
 
-    def _inject_drift(selfself, batch: pd.DataFrame) -> pd.DataFrame:
+    def _inject_drift(self, batch: pd.DataFrame) -> pd.DataFrame:
         # Covariate shift: Fraudsters change tactics, or holiday shopping spikes transaction amounts
         # Inflating 'amount' specifically for TRANSFER types to create a clean statistical anomaly
         mask = (batch['type'] == 'TRANSFER') & (batch['isFraud'] == 0)
-        batch.loc[mask, 'amount'] = batch.loc[mask, 'amount'] * 3.5
+        batch.loc[mask, 'amount'] = batch.loc[mask, 'amount'] * 3.0
         return batch
